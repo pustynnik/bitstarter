2bc5d62f049595946919c51c918f5ccfdf3efb17
Sergey Pustynnik <sergey.pustynnik@gmail.com>
2013-07-10 21:49:05 +0000

    Modified web.js

--- web.js
+++ web.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
-var buffer = new Buffer(256);
 
 app.get('/', function(request, response) {
-  buffer = fs.readFileSync('index.html'));
+  var buffer = new Buffer(fs.readFileSync('index.html'));
   response.send(buffer.toString());
 });
 
